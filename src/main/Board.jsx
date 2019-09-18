6bdcb27e8352c243eec88c034dfaf9bb6f33dc58
Leonardo Petrauskas - Tecnologia da Informação <leonardo.petrauskas@unimedsa.com.br>
2019-09-18 19:40:59 -0300

    Board class

--- src/main/Board.jsx
+++ src/main/Board.jsx
@@ -1,46 +1,70 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Box from '../components/Box';
 import './Board.css';
 let key = 0;
-let b = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0],
+let board = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
-export default props => {
-    let [message, setMessage] = useState('');
-    let [board, setBoard] = useState(b)
-    let boxes = [];
 
-    function changeBoardValue(row, col, value) {
-        b[row][col] = value;
-        setBoard(b)
-        // setMessage('');
-        if (verifyRow(row, value) > 1 || verifyCol(col, value) > 1)
-            // setMessage('Valor inválido')
-            console.log('Valor inválido')
+function Message(props) {
+    return (
+        <div>
+            {props.message}
+        </div>);
+}
+
+export default class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: '',
+            board: '',
+            boxes: []
+        };
+        this.changeBoardValue = this.changeBoardValue.bind(this);
+        this.verifyCol = this.verifyCol.bind(this);
+        this.verifyRow = this.verifyRow.bind(this);
+
     }
 
-    function verifyRow(row, value) {
+    componentWillMount() {
+        let boxes = [];
+        for (let row = 0; row < 9; row++) {
+            for (let col = 0; col < 9; col++) {
+                boxes.push(<Box key={`block${key++}`} col={col} row={row} changeBoardValue={this.changeBoardValue} />);
+            }
+        }
+        this.setState({ boxes, board })
+    }
+
+    changeBoardValue(row, col, value) {
+        let { board } = this.state;
+        board[row][col] = value;
+        if (this.state.verifyRow(row, value) > 1 || this.state.verifyCol(col, value) > 1)
+            this.setState({message: 'Valor inválido'})
+    }
+
+    verifyRow(row, value, board) {
         let result = board[row].filter(val => val === value);
         return result.length;
     }
 
-    function verifyCol(col, value) {
+    verifyCol(col, value, board) {
         let result = board.filter(x => x[col] === value);
         return result.length;
     }
 
-    for (let row = 0; row < 9; row++) {
-        for (let col = 0; col < 9; col++) {
-            boxes.push(<Box key={`block${key++}`} col={col} row={row} changeBoardValue={changeBoardValue} />);
-        }
+    render() {
+        return (
+            <React.Fragment>
+                <Message message={this.state.message} />
+                <div className="board">
+                    {this.state.boxes}
+                </div>
+            </React.Fragment >
+        );
     }
-    return (
-        <React.Fragment>
-            {message}
-            <div className="board">
-                {boxes}
-            </div>
-        </React.Fragment>
-    );
+
+
 }
