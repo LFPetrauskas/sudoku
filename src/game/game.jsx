e1a0b6ab670296a9fe188815f9fb402bc2dc7777
Leonardo F Petrauskas <leonardopetrauskas@hotmail.com>
2019-10-21 22:24:58 -0300

    Mudanças na organização

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -1,57 +1,4 @@
-import React from 'react';
-import Box from '../components/Box';
-
-export function initBoard() {
-    let key = 0;
-    let board = [['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '']];
-    let boxes = [];
-    for (let row = 0; row < 9; row++) {
-        for (let col = 0; col < 9; col++) {
-            let blocked = board[row][col] !== '' ? true : false;
-            let initialValue = board[row][col];
-            boxes.push(<Box key={`block${key++}`} col={col} row={row} changeBoardValue={changeBoardValue} blocked={blocked} value={initialValue} />);
-        }
-    }
-    this.setState({ boxes, board })
-}
-
-// export function changeBoardValue(row, col, value) {
-//     let { board } = this.state;
-//     board[row][col] = value;
-//     this.setState({ board, message: '' })
-//     if (verifyRow(row, this.state.board) > 0 || verifyCol(col, this.state.board) > 0 || verifySqr(row, col, this.state.board) > 0)
-//         this.setState({ message: 'Valor inválido' });
-// }
-
-export function verifyRow(row, board) {
-    return verify(board[row].filter(val => val !== ''));
-}
-
-export function verifyCol(col, board) {
-    let arr = [];
-    for (let i = 0; i < 9; i++) {
-        if (board[i][col] !== '') {
-            arr.push(board[i][col]);
-        }
-    }
-    return verify(arr)
-}
-
-export function verifySqr(row, col, board) {
-    let arr = [];
-    let [x, y] = [Math.floor((row) / 3) * 3, Math.floor((col) / 3) * 3];
-    for (let i = x; i < x + 3; i++) {
-        for (let j = y; j < y + 3; j++) {
-            if (board[i][j] !== '')
-                arr.push(board[i][j]);
-        }
-    }
-    return verify(arr);
-}
-
-export function verify(arr) {
+function verify(arr) {
     let exists = false;
     for (let j = 0; j < arr.length; j++) {
         for (let i = 0; i < arr.length; i++) {
@@ -67,3 +14,46 @@ export function verify(arr) {
         return 1;
     return 0;
 }
+
+function verifyCol(col, board) {
+    let arr = [];
+    for (let i = 0; i < 9; i++) {
+        if (board[i][col] !== '') {
+            arr.push(board[i][col]);
+        }
+    }
+    return verify(arr)
+}
+
+function verifyRow(row, board) {
+    return verify(board[row].filter(val => val !== ''));
+}
+
+function verifySqr(row, col, board) {
+    let arr = [];
+    let [x, y] = [Math.floor((row) / 3) * 3, Math.floor((col) / 3) * 3];
+    for (let i = x; i < x + 3; i++) {
+        for (let j = y; j < y + 3; j++) {
+            if (board[i][j] !== '')
+                arr.push(board[i][j]);
+        }
+    }
+    return verify(arr);
+}
+
+function checkBoard(board, row, col, value) {
+    board[row][col] = value;
+    let message = '';
+    if (verifyRow(row, board) > 0 || verifyCol(col, board) > 0 || verifySqr(row, col, board) > 0)
+        message = 'Valor inválido';
+    return ({board, message});
+}
+
+function initBoard() {
+    let board = [['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '']];
+    return board;
+}
+
+export { initBoard, checkBoard, verifyCol, verifyRow, verifySqr }
--- src/main/Board.jsx
+++ src/main/Board.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Box from '../components/Box';
-// import {initBoard, verify, verifyCol, verifyRow, verifySqr,} from '../game/Game';
+import { initBoard, checkBoard } from '../game/Game';
 import './Board.css';
 
 function Message(props) {
@@ -19,15 +19,11 @@ export default class Board extends React.Component {
             boxes: []
         };
         this.changeBoardValue = this.changeBoardValue.bind(this);
-        this.verifyCol = this.verifyCol.bind(this);
-        this.verifyRow = this.verifyRow.bind(this);
-        this.verify = this.verify.bind(this);
-        this.verifySqr = this.verifySqr.bind(this);
     }
 
     componentDidMount() {
         let key = 0;
-        let board = this.initializeBoard('');
+        let board = initBoard('');
         let boxes = [];
         for (let row = 0; row < 9; row++) {
             for (let col = 0; col < 9; col++) {
@@ -39,111 +35,8 @@ export default class Board extends React.Component {
         this.setState({ boxes, board })
     }
 
-    initializeBoard(difficulty) {
-        // let item = { value: '', possibilities: [1, 2, 3, 4, 5, 6, 7, 8, 9] };
-        // let board = [[item, item, item, item, item, item, item, item, item],
-        // [item, item, item, item, item, item, item, item, item],
-        // [item, item, item, item, item, item, item, item, item],
-        // [item, item, item, item, item, item, item, item, item],
-        // [item, item, item, item, item, item, item, item, item],
-        // [item, item, item, item, item, item, item, item, item],
-        // [item, item, item, item, item, item, item, item, item],
-        // [item, item, item, item, item, item, item, item, item],
-        // [item, item, item, item, item, item, item, item, item]];
-
-        let item = { value: '', possibilities: [1, 2, 3] };
-        let board = [[item, item, item], [item, item, item], [item, item, item]];
-
-        for (let row = 0; row < 3; row++) {
-            for (let col = 0; col < 3; col++) {
-                let i = board[row][col];
-                let lengthp = i.possibilities.length;
-                let s = Math.floor(Math.random() * lengthp);
-                let v = i.possibilities[s];
-                board[row][col].value = v;
-                for (let x = 0; x < 3; x++) {
-                    board[x][col].possibilities = board[x][col].possibilities.filter(e => e !== v);
-                }
-                for (let y = 0; y < 3; y++) {
-                    board[row][y].possibilities = board[row][y].possibilities.filter(e => e !== v);
-                }
-            }
-        }
-
-        console.log(board)
-        // for (let row = 0; row < 9; row++) {
-        //     let possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        //     for (let col = 0; col < 9; col++) {
-        //         let proceed = false;
-        //         while (!proceed) {
-        //             let p = Math.floor(Math.random() * possibilities.length);
-        //             board[row][col] = possibilities[p];
-        //             if (this.verifyCol(col, board) === 0 && this.verifyRow(row, board) === 0 && this.verifySqr(row, col, board) === 0) {
-        //                 proceed = true;
-        //                 board[row][col] = possibilities[p];
-        //             } else {
-        //                 board[row][col] = '';
-        //                 possibilities = possibilities.filter((x, y) => y !== p);
-        //             }
-        //         }
-
-        //     }
-        // }
-        board = [['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '']];
-        return board;
-    }
-
     changeBoardValue(row, col, value) {
-        let { board } = this.state;
-        board[row][col] = value;
-        this.setState({ board, message: '' })
-        if (this.verifyRow(row, this.state.board) > 0 || this.verifyCol(col, this.state.board) > 0 || this.verifySqr(row, col, this.state.board) > 0)
-            this.setState({ message: 'Valor inválido' });
-    }
-
-    verifyRow(row, board) {
-        return this.verify(board[row].filter(val => val !== ''));
-    }
-
-    verifyCol(col, board) {
-        let arr = [];
-        for (let i = 0; i < 9; i++) {
-            if (board[i][col] !== '') {
-                arr.push(board[i][col]);
-            }
-        }
-        return this.verify(arr)
-    }
-
-    verifySqr(row, col, board) {
-        let arr = [];
-        let [x, y] = [Math.floor((row) / 3) * 3, Math.floor((col) / 3) * 3];
-        for (let i = x; i < x + 3; i++) {
-            for (let j = y; j < y + 3; j++) {
-                if (board[i][j] !== '')
-                    arr.push(board[i][j]);
-            }
-        }
-        return this.verify(arr);
-    }
-
-    verify(arr) {
-        let exists = false;
-        for (let j = 0; j < arr.length; j++) {
-            for (let i = 0; i < arr.length; i++) {
-                if (i === j) continue;
-                if (arr[i] === arr[j]) {
-                    exists = true;
-                    break;
-                }
-            }
-        }
-
-        if (exists)
-            return 1;
-        return 0;
+        this.setState(checkBoard(this.state.board, row, col, value));
     }
 
     render() {
@@ -153,9 +46,7 @@ export default class Board extends React.Component {
                 <div className="board">
                     {this.state.boxes}
                 </div>
-            </React.Fragment >
+            </React.Fragment>
         );
     }
-
-
 }
